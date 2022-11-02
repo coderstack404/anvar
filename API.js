ed55d786a846a8cb589ec2f96edaee36c05da3d9
coderstack404 <coderstack404@gmail.com>
2022-11-02 10:18:06 +0500

    ihgi

---
+++ API.js
@@ -0,0 +1,33 @@
+let kinobox = document.querySelector(".kino-box")
+function search_func() {
+    let input = document.querySelector("input")
+    fetch(`https://www.omdbapi.com/?s=${input.value}&apikey=787478be`)
+    // https://kitsu.io/api/edge/anime?filter[text]=Naruto
+    .then(res => res.json())
+    .then(kinolar => {
+        let barchaKino = kinolar.Search
+        kinobox.innerHTML = ""
+        kinobox.style.display = "flex";
+        kinobox.style.flexWrap = "wrap";
+        try {
+            barchaKino.forEach(kino => {
+                let div = document.createElement("div")
+                div.setAttribute("class", "anime")
+                div.innerHTML = `
+                <div class="card m-3" style="width: 18rem;">
+                <img class="card-img-top" src="${kino.Poster}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">${kino.Title}</h5>
+                        <p class="card-text">${kino.Year}</p>
+                        <a href="#" class="btn btn-primary">Anime</a>
+                    </div>
+                </div>
+                `
+                kinobox.appendChild(div)
+            })
+        } catch (e) {
+            alert("Anime topilmadi!!!")
+        }
+        ;
+    })
+}
